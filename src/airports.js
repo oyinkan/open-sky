b80d17e434b79214ec9a4251777351c4eea53f21
oyinkan <skusman@student.lautech.edu.ng>
2021-02-08 11:06:09 +0100

    wordked on the dashboard page - added data for city with highest air traffic

---
+++ src/airports.js
@@ -0,0 +1,62 @@
+export const airportData = [
+    {
+        airportName: "Hartsfield–Jackson Atlanta Airport",
+        icao: "KATL",
+        city: "Atlanta",
+        country: "United States"
+    },
+    {
+        airportName: "Beijing Capital International Airport",
+        icao: "ZBAA",
+        city: "Beijing",
+        country: "China"
+    },
+    {
+        airportName: "Chicago O'Hare Airport",
+        icao: "KORD",
+        city: "Chicago",
+        country: "United States"
+    },
+    {
+        airportName: "London Heathrow Airport",
+        icao: "EGLL",
+        city: "London",
+        country: "United Kingdom"
+    },
+    {
+        airportName: "Haneda Airport",
+        icao: "RJTT",
+        city: "Tokyo",
+        country: "Japan"
+    },
+    {
+        airportName: "Los Angeles International Airport",
+        icao: "KLAX",
+        city: "Los Angeles",
+        country: "United States"
+    },
+    {
+        airportName: "Paris Charles de Gaulle Airport",
+        icao: "LFPG",
+        city: "Paris",
+        country: "France"
+    },
+    {
+        airportName: "Dallas Fort Worth International Airport",
+        icao: "KDFW",
+        city: "Dallas",
+        country: "United States"
+    },
+    {
+        airportName: "Frankfurt Airport",
+        icao: "EDDF",
+        city: "Frankfurt",
+        country: "Germany"
+    },
+    {
+        airportName: "Hong Kong International Airport",
+        icao: "VHHH",
+        city: "Hong Kong",
+        country: "China"
+    }
+]
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,52 +1,98 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import "./Dashboard.css";
+import {airportData} from "./../../airports";
 import Card from '@material-ui/core/Card';
 import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
-    card: {
-        width: '23%',
-        padding: '5rem 3rem',
-        borderRadius: '1rem',
-        marginBottom: '3rem'
+    paper: {
+        position: 'absolute',
+        width: 400,
+        backgroundColor: '#444',
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+        top: '46%',
+        left: '48%',
+        transform: 'translate(-46%, -42%)'
     },
     country: {
         marginTop: '1.5rem',
-        fontSize: '3rem'
+        fontSize: '2.5rem'
+    },
+    modalbutton: {
+        fontSize: '1.4rem',
+        marginTop: '2.5rem',
+        padding: '1.2rem 3rem',
+        backgroundColor: '#557d48'
     },
 }));
 
 export default function Dashboard() {
-    const [flights, setFlights] = useState({});
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
 
-    useEffect(() => {
-        fetch("https://opensky-network.org/api/states/all")
-        .then(response => response.json())
-        .then(data => {
-            setFlights(data);
-        })
+    // useEffect(() => {
+    //     fetch("https://opensky-network.org/api/states/all")
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         setFlights(data);
+    //     })
        
-    }, [])
+    // }, [])
 
-    const allFlights = flights?.states?.map(flight => (
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const allAirports = airportData.map(airport => (
         <Card 
             variant="outlined" 
-            className={classes.card} 
-            key={flight[0]} 
-            style={{width: '23%'}}
+            className="airport-card" 
+            key={airport.icao} 
         >
-            <h3>{flight[1]}</h3>
-            <h1 className={classes.country}>{flight[2]}</h1>
+            <h3>{airport.airportName}</h3>
+            <h1 className={classes.country}>{`${airport.city}, ${airport.country}`}</h1>
+            <Button 
+                variant="contained" 
+                onClick={handleOpen} 
+                color="primary"
+                className={classes.modalbutton}
+            >
+                Open Modal
+            </Button>
         </Card>
-    ))
+    ));
+
+    const body = (
+        <div className={classes.paper}>
+          <h2 id="simple-modal-title">Text in a modal</h2>
+          <p id="simple-modal-description">
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </p>
+        </div>
+    );
 
     return(
         <div className="dashboard-wrapper">
             <h2 className="text-center title">Dashboard</h2>
             <div className="grid">
-                {allFlights}
+                {allAirports}
             </div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+                {body}
+            </Modal>
         </div>
     );
 }
